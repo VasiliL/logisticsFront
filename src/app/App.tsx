7c4874aacde624aa360aee6f2b095e81917a67bb
d.baylo <d.baylo@example.com>
2024-08-27 16:41:02 +0300

    fix websocket

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,7 +6,7 @@ import { LicenseInfo } from '@mui/x-license';
 import AuthStore from '@src/store/AuthStore';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import useWebSocket, { ReadyState } from 'react-use-websocket';
+import useWebSocket from 'react-use-websocket';
 
 const WS_URL = 'ws://testing.rwtarif.com:8000/api/v2/ws';
 
@@ -15,6 +15,7 @@ export default function App() {
   const { refresh } = AuthStore;
 
   const intervalRef = useRef();
+  const token = localStorage.getItem('token');
   const getToken = useCallback(() => {
     // Get new token if and only if existing token is available
     console.log('Обновление токена');
@@ -22,17 +23,28 @@ export default function App() {
       void refresh();
     }
   }, [refresh]);
-  //
-  // const { sendJsonMessage, readyState } = useWebSocket(WS_URL, {
-  //   protocols: [localStorage.getItem('token') || ''],
-  //   onOpen: () => {
-  //     console.log('WebSocket connection established.');
-  //   },
-  //   share: true,
-  //   filter: () => false,
-  //   retryOnError: true,
-  //   shouldReconnect: () => true,
-  // });
+
+  if (token) {
+    const { sendJsonMessage, readyState } = useWebSocket(WS_URL, {
+      protocols: [token],
+      onOpen: () => {
+        console.log('WebSocket connection established.');
+      },
+      onClose: () => {
+        console.log('WebSocket connection closed.');
+      },
+      onMessage(event) {
+        console.log('WebSocket sent event', event);
+      },
+      onError(event) {
+        console.log('WebSocket sent error event', event);
+      },
+      share: true,
+      filter: () => false,
+      retryOnError: true,
+      shouldReconnect: () => true,
+    });
+  }
 
   // Trigger API to get a new token before token gets expired.
   useEffect(() => {
